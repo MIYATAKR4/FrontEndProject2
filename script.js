5e00a4b2fe3c84f22fd00fb9fc7b273b2429f5f5
Catarina Martins Diniz <catarina.cmd@gmail.com>
2023-06-27 11:12:36 -0300

    reorder
    alphabetic order + add a readme

--- script.js
+++ script.js
@@ -1,24 +1,6 @@
 function carregaDict() {
   var bios = {
     bio1: {
-      nome: "Marielle Franco",
-      imagem: "https://i.postimg.cc/8fS8KSGX/marielle-franco.png",
-      descricao:
-        "Marielle Francisco da Silva, conhecida como Marielle Franco, foi uma socióloga, feminista, militante dos direitos humanos e política brasileira. Filiada ao Partido Socialismo e Liberdade (PSOL), elegeu-se vereadora do Rio de Janeiro para a Legislatura 2017-2020, durante a eleição municipal de 2016, com a quinta maior votação. Crítica da intervenção federal no Rio de Janeiro e da Polícia Militar, denunciava constantemente abusos de autoridade por parte de policiais contra moradores de comunidades carentes. Em 14 de março de 2018, foi assassinada a tiros.",
-      citacao:
-        "Quantos mais vão precisar morrer para que essa guerra aos pobres acabe?",
-    },
-
-    bio2: {
-      nome: "Lélia Gonzalez",
-      imagem: "https://i.postimg.cc/2q1f3GTG/lelia-gonzalez.png",
-      descricao:
-        "Lélia Gonzalez foi uma intelectual, política, professora e antropóloga brasileira.Ajudou a fundar instituições como o Movimento Negro Unificado(MNU), o Instituto de Pesquisas das Culturas Negras(IPCN), o Coletivo de Mulheres Negras N'Zinga e o Olodum.Sua militância em defesa da mulher negra levou- a ao Conselho Nacional dos Direitos da Mulher(CNDM), no qual atuou de 1985 a 1989. Foi candidata a deputada federal pelo PT, elegendo- se primeira suplente.Nas eleições seguintes, em 1986, candidatou - se a deputada estadual pelo PDT, novamente elegendo - se suplente.",
-      citacao:
-        "A gente não nasce negro, a gente se torna negro. É uma conquista dura, cruel e que se desenvolve pela vida da gente afora.",
-    },
-
-    bio3: {
       nome: "Angela Davis",
       imagem: "https://i.postimg.cc/LJ6SF21r/angela-davis.png",
       descricao:
@@ -27,7 +9,16 @@ function carregaDict() {
         "Não aceito mais as coisas que não posso mudar, estou mudando as coisas que não posso aceitar.",
     },
 
-    bio4: {
+    bio2: {
+      nome: "Carolina Maria de Jesus",
+      imagem: "https://i.postimg.cc/Fdc5VKMJ/carolina-de-jesus.png",
+      descricao:
+        "Carolina Maria de Jesus foi uma renomada escritora brasileira, reconhecida principalmente pelo seu livro Quarto de Despejo: Diário de uma Favelada, publicado em 1960. Ela é considerada uma das primeiras e mais importantes escritoras negras do Brasil. Durante grande parte de sua vida, a autora residia na favela do Canindé, localizada na zona norte de São Paulo, e sustentava a si mesma e seus três filhos como catadora de papel. Em 1958, seu diário foi publicado com o título Quarto de Despejo, com a colaboração do jornalista Audálio Dantas. O livro alcançou um enorme sucesso e foi traduzido para catorze idiomas diferentes.",
+      citacao:
+        "Ah, comigo o mundo vai modificar-se. Não gosto do mundo como ele é.",
+    },
+    
+    bio3: {
       nome: "Conceição Evaristo",
       imagem: "https://i.postimg.cc/Z9XzNNMS/conceicao-evaristo.png",
       descricao:
@@ -35,14 +26,23 @@ function carregaDict() {
       citacao:
         "O importante não é ser o primeiro ou primeira, o importante é abrir caminhos.",
     },
+    
+    bio4: {
+      nome: "Lélia Gonzalez",
+      imagem: "https://i.postimg.cc/2q1f3GTG/lelia-gonzalez.png",
+      descricao:
+        "Lélia Gonzalez foi uma intelectual, política, professora e antropóloga brasileira.Ajudou a fundar instituições como o Movimento Negro Unificado(MNU), o Instituto de Pesquisas das Culturas Negras(IPCN), o Coletivo de Mulheres Negras N'Zinga e o Olodum.Sua militância em defesa da mulher negra levou- a ao Conselho Nacional dos Direitos da Mulher(CNDM), no qual atuou de 1985 a 1989. Foi candidata a deputada federal pelo PT, elegendo- se primeira suplente.Nas eleições seguintes, em 1986, candidatou - se a deputada estadual pelo PDT, novamente elegendo - se suplente.",
+      citacao:
+        "A gente não nasce negro, a gente se torna negro. É uma conquista dura, cruel e que se desenvolve pela vida da gente afora.",
+    },
 
     bio5: {
-      nome: "Carolina Maria de Jesus",
-      imagem: "https://i.postimg.cc/Fdc5VKMJ/carolina-de-jesus.png",
+      nome: "Marielle Franco",
+      imagem: "https://i.postimg.cc/8fS8KSGX/marielle-franco.png",
       descricao:
-        "Carolina Maria de Jesus foi uma renomada escritora brasileira, reconhecida principalmente pelo seu livro Quarto de Despejo: Diário de uma Favelada, publicado em 1960. Ela é considerada uma das primeiras e mais importantes escritoras negras do Brasil. Durante grande parte de sua vida, a autora residia na favela do Canindé, localizada na zona norte de São Paulo, e sustentava a si mesma e seus três filhos como catadora de papel. Em 1958, seu diário foi publicado com o título Quarto de Despejo, com a colaboração do jornalista Audálio Dantas. O livro alcançou um enorme sucesso e foi traduzido para catorze idiomas diferentes.",
+        "Marielle Francisco da Silva, conhecida como Marielle Franco, foi uma socióloga, feminista, militante dos direitos humanos e política brasileira. Filiada ao Partido Socialismo e Liberdade (PSOL), elegeu-se vereadora do Rio de Janeiro para a Legislatura 2017-2020, durante a eleição municipal de 2016, com a quinta maior votação. Crítica da intervenção federal no Rio de Janeiro e da Polícia Militar, denunciava constantemente abusos de autoridade por parte de policiais contra moradores de comunidades carentes. Em 14 de março de 2018, foi assassinada a tiros.",
       citacao:
-        "Ah, comigo o mundo vai modificar-se. Não gosto do mundo como ele é.",
+        "Quantos mais vão precisar morrer para que essa guerra aos pobres acabe?",
     },
 
     bio6: {
